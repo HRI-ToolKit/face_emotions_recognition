e1aaf7590f69e37f1fbaca8673e3655782384c34
saarCogni <120086907+saarCogni@users.noreply.github.com>
2023-02-28 13:04:10 +0200

    Delete build directory

--- build/emotions_recognition/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/saarmoseri/Documents/Projects/face/face_emotions_recognition/install/emotions_recognition'
--- build/emotions_recognition/share/emotions_recognition/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/saarmoseri/Documents/Projects/face/face_emotions_recognition/build/emotions_recognition"
